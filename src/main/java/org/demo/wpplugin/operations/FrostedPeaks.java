aa5df89748c3d23ef1c15b5cd599d50969a3ff20
IR0NSIGHT <irnsght@googlemail.com>
2024-05-17 16:29:59 +0200

    refactor FrostedPeaks because bug with inheritance

--- src/main/java/org/demo/wpplugin/operations/FrostedPeaks.java
+++ src/main/java/org/demo/wpplugin/operations/FrostedPeaks.java
@@ -1,15 +1,18 @@
 package org.demo.wpplugin.operations;
 
 import org.pepsoft.worldpainter.App;
+import org.pepsoft.worldpainter.MapDragControl;
 import org.pepsoft.worldpainter.WorldPainterDialog;
+import org.pepsoft.worldpainter.WorldPainterView;
 import org.pepsoft.worldpainter.layers.Frost;
-import org.pepsoft.worldpainter.operations.Filter;
-import org.pepsoft.worldpainter.operations.MouseOrTabletOperation;
-import org.pepsoft.worldpainter.operations.PaintOperation;
-import org.pepsoft.worldpainter.operations.StandardOptionsPanel;
+import org.pepsoft.worldpainter.operations.*;
 import org.pepsoft.worldpainter.painting.Paint;
+import org.pepsoft.worldpainter.panels.DefaultFilter;
 
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.beans.PropertyVetoException;
+import java.lang.reflect.Field;
 import java.util.Random;
 
 import static org.pepsoft.worldpainter.Constants.TILE_SIZE;
@@ -19,14 +22,21 @@ import static org.pepsoft.worldpainter.Constants.TILE_SIZE;
  * frost - no-frost will have a smooth, random and deterministic transition
  * Right click to configure the tool
  */
-public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperation
-{
-    public FrostedPeaks() {
-        super(NAME, DESCRIPTION, null, ID, "frostedPeaksIcon");
-        System.out.println("icon name of frosted peaks:"+ this.getIcon());
+public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperation {
+        public FrostedPeaks() {
+        super(NAME, DESCRIPTION, null, ID);
+
+        System.out.println(NAME + " -> icon name of frosted peaks:" + this.getIcon());
+
     }
 
-    private final StandardOptionsPanel optionsPanel = new StandardOptionsPanel(NAME, "<p>Left-Click to apply frost to all mountain peaks with a smooth transition.<br>Right-Click for configuration.<br>by Ir0nsight<p>");
+    private final StandardOptionsPanel optionsPanel = new StandardOptionsPanel(NAME,
+            "<p>Left-Click to apply frost to all mountain peaks with a smooth transition.<br>" +
+                    "0% frost at height 'at or above'.<br>" +
+                    "100% frost at height 'at or below.<br>" +
+                    "Linear transition inbetween both heights.<br>" +
+                    "Will ignore other filters.<br>" +
+                    "by Ir0nsight<p>");
 
     @Override
     public JPanel getOptionsPanel() {
@@ -44,21 +54,45 @@ public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperati
         return (rand < point);
     }
 
+    public static Filter getAppFilter(App appInstance) {
+        try {
+            // Get the Class object for the App class
+            Class<?> appClass = appInstance.getClass();
+
+            // Get the private field mapDragControl
+            Field field = appClass.getDeclaredField("filter");
+
+            // Make the private field accessible
+            field.setAccessible(true);
+
+            // Get the value of the field from the instance
+            return (Filter) field.get(appInstance);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     @Override
     protected void tick(int centreX, int centreY, boolean inverse, boolean first, float dynamicLevel) {
         if (inverse) {
-            System.out.println("right click => configure");
-            popup();
+            //ignored
         } else {
 
-            Filter f = paint.getFilter();
+            Filter f = getAppFilter(App.getInstance());
+
+            final int minHeight = (f instanceof DefaultFilter) ? ((DefaultFilter) f).getAboveLevel() : Integer.MIN_VALUE;
+
+            final int fullHeight = (f instanceof DefaultFilter) ? ((DefaultFilter) f).getBelowLevel() : minHeight + 50;
+
 
             getDimension().visitTilesForEditing().andDo(tile -> {
                 Random r = new Random((long) tile.getX()+tile.getY());
 
                 for (int x = 0; x < TILE_SIZE; x++) {
                     for (int y = 0; y < TILE_SIZE; y++) {
-                        if (isFrosted(x, y, tile.getIntHeight(x, y), 120, 100,r))
+                        if (isFrosted(x, y, tile.getIntHeight(x, y), minHeight, fullHeight - minHeight, r))
                             tile.setBitLayerValue(Frost.INSTANCE, x, y, true);
                     }
                 }
@@ -89,14 +123,13 @@ public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperati
      */
     static final String DESCRIPTION = "Globally apply a layer of frost starting at level 120, with a smooth transition";
 
-    private Paint paint;
     @Override
     public Paint getPaint() {
-        return paint;
+        return null;
     }
 
     @Override
     public void setPaint(Paint paint) {
-        this.paint = paint;
+
     }
 }
