4cec11be0e2fe184cc1a8cf638cc7887b2c6b654
IR0NSIGHT <irnsght@googlemail.com>
2024-08-18 12:23:08 +0200

    use FilteredOperation instead of hacking to get App.filter

--- src/main/java/org/demo/wpplugin/operations/FrostedPeaks.java
+++ src/main/java/org/demo/wpplugin/operations/FrostedPeaks.java
@@ -1,9 +1,8 @@
 package org.demo.wpplugin.operations;
 
-import org.pepsoft.worldpainter.App;
-import org.pepsoft.worldpainter.WorldPainterDialog;
 import org.pepsoft.worldpainter.layers.Frost;
 import org.pepsoft.worldpainter.operations.Filter;
+import org.pepsoft.worldpainter.operations.FilteredOperation;
 import org.pepsoft.worldpainter.operations.MouseOrTabletOperation;
 import org.pepsoft.worldpainter.operations.PaintOperation;
 import org.pepsoft.worldpainter.operations.StandardOptionsPanel;
@@ -13,7 +12,6 @@ import org.pepsoft.worldpainter.selection.SelectionBlock;
 import org.pepsoft.worldpainter.selection.SelectionChunk;
 
 import javax.swing.*;
-import java.lang.reflect.Field;
 import java.util.Random;
 
 import static org.pepsoft.worldpainter.Constants.TILE_SIZE;
@@ -23,7 +21,7 @@ import static org.pepsoft.worldpainter.Constants.TILE_SIZE;
  * frost - no-frost will have a smooth, random and deterministic transition
  * Right click to configure the tool
  */
-public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperation {
+public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperation, FilteredOperation {
         public FrostedPeaks() {
         super(NAME, DESCRIPTION, null, ID);
     }
@@ -53,25 +51,6 @@ public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperati
         return (rand < point);
     }
 
-    public static Filter getAppFilter(App appInstance) {
-        try {
-            // Get the Class object for the App class
-            Class<?> appClass = appInstance.getClass();
-
-            // Get the private field mapDragControl
-            Field field = appClass.getDeclaredField("filter");
-
-            // Make the private field accessible
-            field.setAccessible(true);
-
-            // Get the value of the field from the instance
-            return (Filter) field.get(appInstance);
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 
     @Override
     protected void tick(int centreX, int centreY, boolean inverse, boolean first, float dynamicLevel) {
@@ -79,7 +58,7 @@ public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperati
             //ignored
         } else {
 
-            Filter f = getAppFilter(App.getInstance());
+            Filter f = this.filter;
 
             if (!(f instanceof DefaultFilter))
                 return;
@@ -110,12 +89,6 @@ public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperati
         return getDimension().getBitLayerValueAt(SelectionChunk.INSTANCE, x, y) || getDimension().getBitLayerValueAt(SelectionBlock.INSTANCE, x, y);
     }
 
-    public void popup() {
-        WorldPainterDialog dialog = new ConfigurationDialog(App.getInstance());
-        dialog.setLocationRelativeTo(App.getInstance());
-        dialog.setVisible(true);
-    }
-
 
     /**
      * The globally unique ID of the operation. It's up to you what to use here. It is not visible to the user. It can
@@ -141,5 +114,16 @@ public class FrostedPeaks extends MouseOrTabletOperation implements PaintOperati
     @Override
     public void setPaint(Paint paint) {
 
+    }
+
+    private Filter filter;
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    @Override
+    public void setFilter(Filter filter) {
+        this.filter = filter;
     }
 }
